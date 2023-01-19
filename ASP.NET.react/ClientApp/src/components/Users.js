436a7ddf03754c6f99efdc69d068f235a65702be
Nykolai <1488nikolay1488@gmail.com>
2023-01-19 15:13:41 +0200

    First commit

---
+++ ASP.NET.react/ClientApp/src/components/Users.js
@@ -0,0 +1,38 @@
+﻿import React, { useState, useEffect } from 'react';
+import { Button } from '../../node_modules/reactstrap/types/index';
+
+
+const Users = () => {
+
+    const [users, setUsers] = useState([]);
+    const userName = 'Namerek';
+
+    useEffect(() => {
+        fetchFromAPI();
+    }, [userName]);
+
+    function fetchFromAPI() {
+        fetch(`user/${userName}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setUsers(data);
+            })
+    }
+
+
+    return (
+        (users != null) ?
+            <main>
+            {
+                    users.map((user) => <h3 key={`user-${user.id}`}>{user.name}</h3>)
+                    <button></button>
+            }
+            </main>
+            : <main><h3>Loading...</h3></main>
+        )
+}
+
+
+export default Users;
---
+++ ASP.NET.react/Models/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET.react.Models
+{
+	public class Company
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public List<UserModel> Users { get; set; }
+	}
+}
